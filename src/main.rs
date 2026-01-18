b3d3d86ec2ecb7502b5275f7a3261e7a8f6def39
XanderDhondtUCLL <xander.dhondt@live.be>
2026-01-18 17:45:49 +0100

    added error handling and negative shifting.

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,25 @@ fn char_encypher(byte: u8, shift: u8) -> char {
     return c as char;
 }
 
+//
+// Function that shifts a byte representing an ASCII character
+// byte: u8
+// shift: u8
+//
+// Similar to encyphering but this time shifting to decypher
+//
+fn char_decypher(byte: u8, shift: u8) -> char {
+    let c = match byte {
+        // uppercase
+        b'A'..=b'Z' => b'A' + (byte - b'A' - shift) % 26,
+        // lowercase
+        b'a'..=b'z' => b'a' + (byte - b'a' - shift) % 26,
+        // everything else
+        _ => byte,
+    };
+    return c as char;
+}
+
 fn main() -> io::Result<()> {
     // file IO
     let input_path: String = fs::read_to_string("data/input.txt")?;
@@ -32,12 +51,28 @@ fn main() -> io::Result<()> {
     // CLI IO
     let mut input: String = String::new();
     io::stdin().read_line(&mut input)?;
-
-    let shift: u8 = input.trim().parse().unwrap();
+    let mut shift: u8;
 
     //
-    // NEED TO HANDLE NEGATIVE SHIFTS HERE
+    // ensure that program doesn't panic when alphabetical characters are inputted instead of shift
     //
+    if input.chars().any(|c| c.is_ascii_alphabetic()) {
+        eprintln!("Can only use numbers to input shift");
+        return Ok(());
+    }
+
+    //
+    // handling of negative shifts
+    // if it starts negatively, since working with u8, do 26 - shift.
+    // Has same effect as taking negative with i8
+    //
+    if input.starts_with("-") {
+        input.remove(0);
+        shift = input.trim().parse().unwrap();
+        shift = 26 - shift;
+    } else {
+        shift = input.trim().parse().unwrap();
+    }
 
     let mut output_data = String::new();
 
