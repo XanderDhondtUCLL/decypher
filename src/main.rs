1773cd156963385601f847e8dc46be0ccb1235c4
XanderDhondtUCLL <xander.dhondt@live.be>
2026-01-21 11:25:35 +0100

    removed hello world, started on own to try and learn

--- src/main.rs
+++ src/main.rs
@@ -5,39 +5,14 @@ use gpui::{
 use std::fs;
 use std::io;
 
-struct HelloWorld {
+struct ViewData {
     text: SharedString,
 }
 
-impl Render for HelloWorld {
-    fn render(&mut self, _window: &mut Window, _cx: &mut Context<Self>) -> impl IntoElement {
-        div()
-            .flex()
-            .flex_col()
-            .gap_3()
-            .bg(rgb(0x505050))
-            .size(px(500.0))
-            .justify_center()
-            .items_center()
-            .shadow_lg()
-            .border_1()
-            .border_color(rgb(0x0000ff))
-            .text_xl()
-            .text_color(rgb(0xffffff))
-            .child(format!("Hello, {}!", &self.text))
-            .child(
-                div()
-                    .flex()
-                    .gap_2()
-                    .child(div().size_8().bg(gpui::red()))
-                    .child(div().size_8().bg(gpui::green()))
-                    .child(div().size_8().bg(gpui::blue()))
-                    .child(div().size_8().bg(gpui::yellow()))
-                    .child(div().size_8().bg(gpui::black()))
-                    .child(div().size_8().bg(gpui::white())),
-            )
-    }
+impl Render for ViewData {
+    fn render(&mut self, window: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {}
 }
+
 //
 // Function that shifts a byte representing an ASCII character
 // byte: u8
@@ -106,22 +81,6 @@ fn decypher(input_file: String, output_path: &str, shift: u8) -> io::Result<()> 
 }
 
 fn main() -> io::Result<()> {
-    Application::new().run(|cx: &mut App| {
-        let bounds = Bounds::centered(None, size(px(500.), px(500.0)), cx);
-        cx.open_window(
-            WindowOptions {
-                window_bounds: Some(WindowBounds::Windowed(bounds)),
-                ..Default::default()
-            },
-            |_, cx| {
-                cx.new(|_| HelloWorld {
-                    text: "World".into(),
-                })
-            },
-        )
-        .unwrap();
-    });
-
     // file IO
     let input_file: String = fs::read_to_string("data/input.txt")?;
     let output_path: &str = "data/output.txt";
